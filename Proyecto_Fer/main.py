4d8a9573d87c24a359eeaee90b305469e38d1131
Fer <fernandodiazmachado@gmail.com>
2020-04-07 19:12:21 -0300

    Creo carpeta personal para trabajar

---
+++ Proyecto_Fer/main.py
@@ -0,0 +1,14 @@
+from curses import wrapper
+
+def main(stdscr):
+	# Clear screen
+	stdscr.clear()
+	
+	s = "Hola curses"	
+	for i in range(len(s)):
+		stdscr.addstr(i, i, s[i])
+
+	stdscr.refresh()
+	stdscr.getkey()
+
+wrapper(main)

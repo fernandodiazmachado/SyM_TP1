5b0eadf3da531adbfc15d372f1fbfc033b763ba6
Fer <fernandodiazmachado@gmail.com>
2020-04-08 12:25:54 -0300

    creo funcion ver solo claves del tablero

--- Proyecto_Fer/tablero.py
+++ Proyecto_Fer/tablero.py
@@ -1,18 +1,32 @@
 def crearTablero():
+    primeraClave=1
+    ultimaClave=25
     diccionario={}
-    for i in range(1,26):
+    for i in range(primeraClave,ultimaClave+1):
+        #el primer valor de clave es incluyente, pero el ultimo excluyente, por eso sumo 1
         diccionario[i]="X"
+        #Seteo todos los valores iguales
     return diccionario
 
-def imprimirTablero():
+def imprimirTableroValores():
     dic = crearTablero()
-    print(dic)
-    corte=5
+    corte=5 
+    #Variable corte, pensada como un salto de linea cada 5 columnas
     for i in dic:
         if i==corte or i==corte*2 or i==corte*3 or i==corte*4:
             print(dic[i])
         else:
             print(dic[i]+"",end="")
+    print()
+
+def imprimirTableroClaves():
+    dic = crearTablero()
+    corte=5
+    for i in dic:
+        if i==corte or i==corte*2 or i==corte*3 or i==corte*4:
+            print(i)
+        else:
+            print(i,end=" ")
     print()
 
 def imprimirTableroCompleto():
@@ -24,5 +38,7 @@ def imprimirTableroCompleto():
             print(i,end="")
     print()
 
-
+imprimirTableroValores()
+imprimirTableroClaves()
+imprimirTableroCompleto()
 

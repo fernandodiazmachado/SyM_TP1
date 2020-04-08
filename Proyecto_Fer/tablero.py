a287fd605882a72759e839079c50e69f0be2018b
Unknown <unknown@example.com>
2020-04-08 01:48:15 -0300

    actualizo tablero

--- Proyecto_Fer/tablero.py
+++ Proyecto_Fer/tablero.py
@@ -5,11 +5,18 @@ listaKeys = tuplaKeys.crearListaDeTuplasKey()
 def crearTablero(listaKeys):
     diccionario={}
     for i in range(1,26):
-        diccionario[listaKeys[i-1]]=""
+        diccionario[i]=""
     return diccionario
 
 dic = crearTablero(listaKeys)
 
-print (dic)
+for i in dic:
+    print(dic[i]+"",end="")
+    if i!=5:
+        print(dic[i]+"",end="")
+    else:
+        print(dic[i])
+
+#print (dic)
 
 

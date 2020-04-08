8a46a5fe031dc74b868be3d96a88557b22b913d8
Unknown <unknown@example.com>
2020-04-08 03:17:12 -0300

    def verTablero

--- Proyecto_Fer/tablero.py
+++ Proyecto_Fer/tablero.py
@@ -1,20 +1,28 @@
-import tuplaKeys 
-
-listaKeys = tuplaKeys.crearListaDeTuplasKey()
-
-def crearTablero(listaKeys):
+def crearTablero():
     diccionario={}
     for i in range(1,26):
-        diccionario[i]="x"
+        diccionario[i]="X"
     return diccionario
 
-dic = crearTablero(listaKeys)
-print(dic)
-corte=5
-for i in dic:
-    if i==corte or i==corte*2 or i==corte*3 or i==corte*4:
-        print(dic[i])
-    else:
-        print(dic[i]+"",end="")
+def imprimirTablero():
+    dic = crearTablero()
+    print(dic)
+    corte=5
+    for i in dic:
+        if i==corte or i==corte*2 or i==corte*3 or i==corte*4:
+            print(dic[i])
+        else:
+            print(dic[i]+"",end="")
+    print()
+
+def imprimirTableroCompleto():
+    dic = crearTablero()
+    for i in dic.items():
+        if i==(5,"X") or i==(10,"X") or i==(15,"X") or i==(20,"X") or i==(5,"") or i==(10,"") or i==(15,"") or i==(20,""):
+            print(i)
+        else:
+            print(i,end="")
+    print()
+
 
 

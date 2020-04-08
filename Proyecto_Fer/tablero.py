7e167ea83460a65c00768c77d8323e8b00603678
Fer <fernandodiazmachado@gmail.com>
2020-04-08 01:25:52 -0300

    actualizo

--- Proyecto_Fer/tablero.py
+++ Proyecto_Fer/tablero.py
@@ -1,27 +1,15 @@
-tuplaKey_1=(1,1)
-tuplaKey_2=(1,2)
-tuplaKey_3=(1,3)
-tuplaKey_4=(1,4)
-tuplaKey_5=(1,5)
+import tuplaKeys 
 
-tuplaKey_6=(2,1)
-tuplaKey_7=(2,2)
-tuplaKey_8=(2,3)
-tuplaKey_9=(2,4)
-tuplaKey_10=(2,5)
+listaKeys = tuplaKeys.crearListaDeTuplasKey()
 
-def crearTablero(): return {
-                            tuplaKey_1:"a",tuplaKey_2:"X",tuplaKey_3:"X",tuplaKey_4:"c",tuplaKey_5:"b",
-                            tuplaKey_6:"X",tuplaKey_7:"X",tuplaKey_8:"X",tuplaKey_9:"X",tuplaKey_10:"X"
-                            }
+def crearTablero(listaKeys):
+    diccionario={}
+    for i in range(1,26):
+        diccionario[listaKeys[i-1]]=""
+    return diccionario
 
-tabla = crearTablero()
+dic = crearTablero(listaKeys)
 
-
-for i in range(1,3):
-    for j in range(1,6):
-        numero=i,j
-        print(tabla[numero]+" ",end="")
-    print()
+print (dic)
 
 
